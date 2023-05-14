04998dcb99666ad5aa2c999ddc4564aed076e3e1
Cliffia123 <Cliffia7@gmail.com>
2023-05-14 10:14:11 +0800

    Update train_cub_cls.sh

--- scripts/train_cub_cls.sh
+++ scripts/train_cub_cls.sh
@@ -3,7 +3,7 @@ gpu=0,1
 CUDA_VISIBLE_DEVICES=${gpu} python3 -m torch.distributed.launch --nproc_per_node=2 --master_port=11241 --use_env main_classifier.py \
 --model deit_small_lctr \
 --batch-size 128 \
---data-path /GPUFS/nsccgz_ywang_zfd/caoxz/data/CUB_200_2011/images \
+--data-path /data/CUB_200_2011/images \
 --output_dir log \
 --resume log/deit_small_patch16_224.pth \
 --epochs 300 \
