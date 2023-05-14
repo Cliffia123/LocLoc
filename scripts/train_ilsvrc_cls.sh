0eb89a68751b9634757946a9e980f4f7f8f7626f
Cliffia123 <Cliffia7@gmail.com>
2023-05-14 10:33:13 +0800

    Update train_ilsvrc_cls.sh

--- scripts/train_ilsvrc_cls.sh
+++ scripts/train_ilsvrc_cls.sh
@@ -6,6 +6,6 @@ CUDA_VISIBLE_DEVICES=${gpu} python3 -m torch.distributed.launch --nproc_per_node
 --data-path ImageNet/train/ILSVRC2012_img_train/data \
 --output_dir log \
 --resume log/deit_small_patch16_224.pth \
---epochs 150 \
+--epochs 50 \
 --data-set IMNET \
 --lr 0.00015
