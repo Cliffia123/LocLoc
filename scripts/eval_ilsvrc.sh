e6684caf0ff57df3ada7b6048f9f5694a1c83913
Cliffia123 <Cliffia7@gmail.com>
2023-05-14 10:13:56 +0800

    Update eval_ilsvrc.sh

--- scripts/eval_ilsvrc.sh
+++ scripts/eval_ilsvrc.sh
@@ -2,7 +2,7 @@ python3 main_eval.py \
 --eval \
 --model deit_small_lctr \
 --data-set IMNET \
---data-path /GPUFS/nsccgz_ywang_zfd/ImageNet/train/ILSVRC2012_img_train/data \
+--data-path ImageNet/train/ILSVRC2012_img_train/data \
 --resume log/deit_small_patch16_224.pth \
 --classifier log/IMNET_cls_best_50.pth \
 --locator log/IMNET_loc_best_50.pth
